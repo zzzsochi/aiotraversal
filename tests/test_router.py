12aedf98dca05981b651dee194d3e7db722987ba
Alexander Zelenyak <zzz.sochi@gmail.com>
2015-05-18 17:09:02 +0300

    Else tests...

--- tests/test_router.py
+++ tests/test_router.py
@@ -1,7 +1,9 @@
-from unittest.mock import Mock
+from unittest.mock import Mock, patch
 
 import pytest
+from aiohttp.web_exceptions import HTTPNotFound
 
+from aiotraversal.exceptions import ViewNotResolved
 from aiotraversal.router import Router, MatchInfo
 from aiotraversal.traversal import (
     find_root,
@@ -47,12 +49,38 @@ def test_traverse_with_tail(loop, root, router):
     assert find_root(res) is root
 
 
+def test_traverse_root_with_tail(loop, root, router):
+    request = Mock(name='request')
+    request.path = '/not/c'
+
+    res, tail = loop.run_until_complete(router.traverse(request))
+
+    assert tail == ('not', 'c')
+    assert len(list(lineage(res))) == 1
+    assert res is root
+
+
 def test_resolve(loop, app, router):
     app.resolve_view.return_value = 'view'
     request = Mock(name='request')
     request.path = '/a/b/not/c'
 
-    mi = loop.run_until_complete(router.resolve(request))
+    with patch('aiotraversal.traversal.traverse') as traverse:
+        mi = loop.run_until_complete(router.resolve(request))
 
+    assert not traverse.called
     assert isinstance(mi, MatchInfo)
     assert mi._view == 'view'
+
+
+def test_resolve_not_found(loop, app, router):
+    class VNRMockException(ViewNotResolved):
+        def __init__(self):
+            pass
+
+    app.resolve_view.side_effect = VNRMockException
+    request = Mock(name='request')
+    request.path = '/a/b/not/c'
+
+    with pytest.raises(HTTPNotFound):
+        loop.run_until_complete(router.resolve(request))
