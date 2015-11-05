98ef8ea5a372bd920a43b390a06421dd14b23c00
Alexander Zelenyak <zzz.sochi@gmail.com>
2015-11-05 14:34:54 +0300

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='aiotraversal',
-      version='0.4',
+      version='0.5',
       description='Traversal based asyncronious web framework',
       # long_description=README,
       classifiers=[
